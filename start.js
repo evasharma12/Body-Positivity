ed411a3ccbb94253144e78aee3c0439570948c3d
Eva Sharma <79697910+evasharma12@users.noreply.github.com>
2021-08-05 19:12:00 +0530

    Update start.js

--- start.js
+++ start.js
@@ -21,6 +21,6 @@ const app = require('./app');
 
 require('./models/Contacts');
 
-const server = app.listen(80, ()=>{
+const server = app.listen(3000, ()=>{
     console.log(`Express is running on port ${server.address().port}`);
 })
